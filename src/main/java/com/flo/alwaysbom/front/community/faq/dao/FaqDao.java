f2412ec925fef4ef6064e40e1d47aae830d76ecb
xzllxz456 <xzllxz456@naver.com>
2021-03-23 10:26:52 +0900

    수정

--- src/main/java/com/flo/alwaysbom/front/community/faq/dao/FaqDao.java
+++ src/main/java/com/flo/alwaysbom/front/community/faq/dao/FaqDao.java
@@ -1,9 +1,17 @@
 package com.flo.alwaysbom.front.community.faq.dao;
 
+
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class FaqDao {
+    @Autowired
     private final SqlSessionTemplate sqlSessionTemplate;
+
+    public FaqDao(SqlSessionTemplate sqlSessionTemplate) {
+        this.sqlSessionTemplate = sqlSessionTemplate;
+    }
+
 }
