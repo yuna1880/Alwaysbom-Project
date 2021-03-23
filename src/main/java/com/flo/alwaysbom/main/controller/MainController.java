82d64c9d0b0eba9d8b5db391fb4a54d62cfc357d
cothis <minho1030@naver.com>
2021-03-23 17:39:18 +0900

    추가 : b_index.jsp

--- src/main/java/com/flo/alwaysbom/main/controller/MainController.java
+++ src/main/java/com/flo/alwaysbom/main/controller/MainController.java
@@ -1,13 +1,27 @@
 package com.flo.alwaysbom.main.controller;
 
+import com.flo.alwaysbom.main.service.MainService;
+import com.flo.alwaysbom.main.vo.MainVo;
+import com.flo.alwaysbom.util.FileHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final MainService mainService;
+    private final FileHandler fileHandler;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        List<MainVo> images = mainService.findImages();
+
+        model.addAttribute("images", images);
         return "main/index";
     }
 }
