fc8ca31489633da73861ada04d13a26337913ac9
cothis <minho1030@naver.com>
2021-03-23 10:19:11 +0900

    프로젝트 : 프로젝트 파일 및 패키지 구조 변경

---
+++ src/main/java/com/flo/alwaysbom/fclass/vo/BranchVo.java
@@ -0,0 +1,16 @@
+package com.flo.alwaysbom.fclass.vo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BranchVo {
+    public Integer idx;
+    public String name;
+    public String color;
+    public String addr;
+    public String mapImage;
+}
