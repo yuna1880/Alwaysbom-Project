7ea68142e49ba016c402e1c35541868156bf4c27
xzllxz456 <xzllxz456@naver.com>
2021-03-23 18:09:09 +0900

    faq : 추가

--- src/main/java/com/flo/alwaysbom/community/faq/dao/FaqDao.java
+++ src/main/java/com/flo/alwaysbom/community/faq/dao/FaqDao.java
@@ -1,16 +1,23 @@
 package com.flo.alwaysbom.community.faq.dao;
 
+import com.flo.alwaysbom.community.faq.vo.FaqVo;
+import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class FaqDao {
-    @Autowired
     private final SqlSessionTemplate sqlSessionTemplate;
 
-    public FaqDao(SqlSessionTemplate sqlSessionTemplate) {
-        this.sqlSessionTemplate = sqlSessionTemplate;
+    public List<String> faqCategoryStrings() {
+        return sqlSessionTemplate.selectList("faq.faqCategoryStrings");
     }
 
+    public List<FaqVo> faqList(FaqVo vo) {
+        return sqlSessionTemplate.selectList("faq.faqlist", vo);
+    }
 }
