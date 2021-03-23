65b6d5c82a755b8cee9258d1ae44cbfc35895fc6
이담영 <Greedysoo@yidamyeong-MacBook-Pro.local>
2021-03-24 04:38:02 +0900

    추가 : 꽃다발 레이아웃

---
+++ src/main/java/com/flo/alwaysbom/product/ProductController.java
@@ -0,0 +1,15 @@
+package com.flo.alwaysbom.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+
+    @GetMapping("/product")
+    public String product() {
+        return "/product/list";
+    }
+}
