1a02706b46542d285d1b6c46831f98df7cf0f5a0
yuna1880 <72509037+yuna1880@users.noreply.github.com>
2021-03-23 10:23:06 +0900

    추가 : subsController

---
+++ src/main/java/com/flo/alwaysbom/front/subs/SubsController.java
@@ -0,0 +1,30 @@
+package com.flo.alwaysbom.front.subs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SubsController {
+
+    private SubsController subsController;
+
+//    public SubsController(SubsController subsController) {
+//      this.subsController = subsController;
+//       System.out.println(">>IndexController() 객체생성");
+//   }
+
+    @GetMapping("goIndex")
+    public String goIndex () {
+        System.out.println("여기");
+        return "subs/main";
+    }
+
+
+
+
+
+
+
+
+}
