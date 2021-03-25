3fdd7a6bed3b48e0626fac483e601b119cccdd0f
cothis <minho1030@naver.com>
2021-03-25 17:29:01 +0900

    추가 : CartDao 및 CartService에 findByIdxArray 기능 추가

--- src/main/java/com/flo/alwaysbom/cart/controller/CartController.java
+++ src/main/java/com/flo/alwaysbom/cart/controller/CartController.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -21,5 +24,23 @@ public class CartController {
 
         model.addAttribute("list", list);
         return "cart/list";
+    }
+
+    @GetMapping("/cart/order")
+    public String order(@RequestBody List<CartVo> list) {
+        System.out.println("list = " + list);
+        for (CartVo cartVo : list) {
+            System.out.println("cartVo = " + cartVo);
+            System.out.println("cartVo.getLetter() = " + cartVo.getLetter());
+        }
+        return "";
+    }
+    @GetMapping("/cart/order2")
+    public String order2(Integer[] idx) {
+        System.out.println("idx = " + Arrays.toString(idx));
+        List<CartVo> list = cartService.findByIdxArray(idx);
+        System.out.println("list = " + list);
+
+        return "";
     }
 }
