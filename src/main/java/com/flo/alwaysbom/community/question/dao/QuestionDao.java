fbd8588991910783857806e16bccaf09ae6ad41d
xzllxz456 <xzllxz456@naver.com>
2021-03-26 17:45:25 +0900

    추가 : MYQ

--- src/main/java/com/flo/alwaysbom/community/question/dao/QuestionDao.java
+++ src/main/java/com/flo/alwaysbom/community/question/dao/QuestionDao.java
@@ -5,11 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class QuestionDao {
     private final SqlSessionTemplate sessionTemplate;
     public void addQuestion(QuestionVo vo) {
         sessionTemplate.insert("question.addque", vo);
+    }
+
+    public List<QuestionVo> userQuestion() {
+        return sessionTemplate.selectList("question.allQuestion");
     }
 }
