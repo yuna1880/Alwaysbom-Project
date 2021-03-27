92a77901e9e193870d65bfcfedcf578da9318ba6
cothis <minho1030@naver.com>
2021-03-27 11:02:33 +0900

    수정 : 각 vo에 최종 금액 계산로직 추가

--- src/main/java/com/flo/alwaysbom/flower/vo/FlowerVo.java
+++ src/main/java/com/flo/alwaysbom/flower/vo/FlowerVo.java
@@ -20,4 +20,10 @@ public class FlowerVo {
     private String content;
     private String delStatus;
     private int freeDelivery;
+
+    /* 핵심 비즈니스 로직 */
+    // 최종 금액 출력
+    public Integer getFinalPrice(){
+        return (int)(price * (1 - discountRate / 100.0));
+    }
 }
