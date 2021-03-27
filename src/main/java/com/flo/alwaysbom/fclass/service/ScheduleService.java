8cd04ecc10c318ea2ae861614a9606fdeb31e78d
Hana <dlagksk64@naver.com>
2021-03-27 21:35:28 +0900

    추가 : 클래스 일정 추가하기

--- src/main/java/com/flo/alwaysbom/fclass/service/ScheduleService.java
+++ src/main/java/com/flo/alwaysbom/fclass/service/ScheduleService.java
@@ -5,6 +5,8 @@ import com.flo.alwaysbom.fclass.vo.ScheduleVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
@@ -12,5 +14,9 @@ public class ScheduleService {
 
     public ScheduleVo addSchedule(ScheduleVo vo) {
         return scheduleDao.addSchedule(vo);
+    }
+
+    public List<ScheduleVo> searchSchedule(ScheduleVo vo) {
+        return scheduleDao.searchSchedule(vo);
     }
 }
