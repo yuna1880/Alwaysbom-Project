84c61a66b32285e1ae99a3b5f5960b746d4eea57
yuna1880 <72509037+yuna1880@users.noreply.github.com>
2021-03-31 10:41:09 +0900

    추가:커밋 전 추가

--- src/main/java/com/flo/alwaysbom/order/controller/OrdersController.java
+++ src/main/java/com/flo/alwaysbom/order/controller/OrdersController.java
@@ -11,16 +11,17 @@ import com.flo.alwaysbom.order.vo.OrdersVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@SessionAttributes({"orderList"})
 public class OrdersController {
 
     private final OrdersService ordersService;
@@ -30,26 +31,38 @@ public class OrdersController {
     // post 가 될 거다....
     //장바구니에 담긴 내용들 가지고 (세션에 저장) -> 편지
     @GetMapping("/order/getLetter")
-    public String goLetter(Integer[] idx, HttpSession session) {
+    public String goLetter(Integer[] idx, Model model) {
         System.out.println("idx = " + Arrays.toString(idx));
         List<CartVo> list = cartService.findByIdxArray(idx);
+        System.out.println("장바구니 list = " + list);
+        model.addAttribute("orderList", list);
+        return "order/letter";
+    }
+
+    @GetMapping("/order/sessionExample")
+    public String sessionExample(@SessionAttribute("orderList") List<CartVo> list) {
         System.out.println("list = " + list);
-        session.setAttribute("OrderList", list);
         return "order/letter";
     }
 
     //배송지입력 페이지 -> 결제전 확인 페이지
     @PostMapping("/order/payment")
-    public String goPayment(OrdersVo vo, OitemVo ovo, MemberVO mvo, Model model) {
+    public String goPayment(@SessionAttribute("orderList") CartVo cvo ,OrdersVo vo, OitemVo ovo, MemberVO mvo, Model model) {
         System.out.println("orderVo = " + vo);
         System.out.println("oItemVo = " + ovo);
+        //id 임시 설정
         mvo.setId("yuna1880");
-        System.out.println("MemverVo = " + mvo);
-        //System.out.println("letterList = " + list);
+        //카트세션 가져오기
+        System.out.println("cvo =" + cvo);
+
+        //vo, ovo 셋팅
+
+
+        System.out.println("MemberVo = " + mvo);
+
 
         //해당 id 회원 포인트 찾기
         int point = ordersService.getPoint(mvo);
-
         model.addAttribute("point",point);
         model.addAttribute("vo", vo);
         model.addAttribute("ovo", vo);
@@ -67,7 +80,6 @@ public class OrdersController {
         DeliveryInfoVo dvo = ordersService.findAddress(vo);
         System.out.println("값 : " + dvo);
         return dvo;
-        // {memberid : yuna1880}
     }
 
 }
