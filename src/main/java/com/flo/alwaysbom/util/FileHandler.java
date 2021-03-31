46f4d8c1148c5143f262493be10facd331007140
cothis <minho1030@naver.com>
2021-04-01 01:13:17 +0900

    추가 : 파일 업로드 방식 클라우드로 변경

--- src/main/java/com/flo/alwaysbom/util/FileHandler.java
+++ src/main/java/com/flo/alwaysbom/util/FileHandler.java
@@ -1,48 +1,44 @@
 package com.flo.alwaysbom.util;
 
-import lombok.RequiredArgsConstructor;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.*;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class FileHandler {
 
-    private final ServletContext context;
+    private final String BUCKET_NAME = "upload.alwaysbom.xyz";
+    private final Storage storage;
+
+    public FileHandler() throws IOException {
+        Resource resource = new DefaultResourceLoader().getResource("classpath:storage-credential.json");
+        this.storage = StorageOptions.newBuilder()
+                .setCredentials(ServiceAccountCredentials.fromStream(resource.getInputStream()))
+                .build()
+                .getService();
+    }
 
     public String uploadFile(MultipartFile file, String dbName, String uploadFolder) throws IOException {
         if (!file.isEmpty()) {
+
             String oriName = file.getOriginalFilename();
             int dotIndex = oriName.lastIndexOf("."); //adf..sf.d.sdafdfd.jpg
             if (dotIndex >= 0 && dotIndex < oriName.length() - 1) {
-                String ext = oriName.substring(dotIndex);
-                String fileName = UUID.randomUUID().toString() + ext;
+                String fileName = new File(new File("/", uploadFolder), oriName).getPath().substring(1);
 
-                File folder = new File("static/upload", uploadFolder);
-                String folderPath = context.getRealPath(folder.getPath());
-                System.out.println(folderPath);
-                //폴더가 있는지 체크
-                File f = new File(folderPath);
-                if (!f.exists()) {
-                    f.mkdirs();
-                }
+                List<Acl> acls = new ArrayList<>();
+                acls.add(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
+                Blob blob = storage.create(BlobInfo.newBuilder(BUCKET_NAME, fileName).setAcl(acls).build(), file.getBytes());
 
-                file.transferTo(new File(folderPath, fileName));
-
-                //기존 파일이 있다면 지우겠다
-                if (dbName != null) {
-                    File dbFile = new File(folderPath, dbName);
-                    if (dbFile.exists()) {
-                        dbFile.delete();
-                    }
-                }
-
-                return new File(new File("/", uploadFolder), fileName).getPath().substring(1);
+                return blob.getMediaLink();
             } else {
                 System.out.println(".이 없거나 확장자의 길이가 1보다 작습니다");
                 return null;
