c1f74ba11a4def5252886e794d88ead8d8381cee
cothis <minho1030@naver.com>
2021-04-01 18:57:53 +0900

    추가 : 쿠폰 매퍼

--- src/main/java/com/flo/alwaysbom/coupon/controller/CouponController.java
+++ src/main/java/com/flo/alwaysbom/coupon/controller/CouponController.java
@@ -1,13 +1,24 @@
 package com.flo.alwaysbom.coupon.controller;
 
+import com.flo.alwaysbom.coupon.service.CouponService;
+import com.flo.alwaysbom.coupon.vo.CouponVo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class CouponController {
 
+    private final CouponService couponService;
+
     @GetMapping("/coupon")
-    public String goCoupon() {
+    public String goCoupon(Model model) {
+        List<CouponVo> list = couponService.findAll();
+        model.addAttribute("couponList", list);
         return "coupon/list";
     }
 }
---
+++ src/main/java/com/flo/alwaysbom/coupon/vo/CouponVo.java
@@ -0,0 +1,21 @@
+package com.flo.alwaysbom.coupon.vo;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
+public class CouponVo {
+
+    private Integer idx;
+    private String name;
+    private String memberId;
+    private Integer status;
+    private Integer point;
+    private Date cdate;
+}
