f53de64b7af3d786d997285b011b0bc4914441b3
2dam0 <yidamyeong@gmail.com>
2021-04-09 11:02:14 +0900

    수정 : 꽃다발에서 장바구니 눌렀을 때 링크 이동 막아둠

--- src/main/java/com/flo/alwaysbom/banner/dao/BannerDao.java
+++ src/main/java/com/flo/alwaysbom/banner/dao/BannerDao.java
@@ -1,14 +1,36 @@
 package com.flo.alwaysbom.banner.dao;
 
+import com.flo.alwaysbom.banner.vo.BannerVo;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class BannerDao {
 
     private final SqlSessionTemplate sqlSessionTemplate;
+
+    public BannerVo addBanner(BannerVo vo) {
+        sqlSessionTemplate.insert("BANNER.addBanner", vo);
+        return vo;
+    }
+
+    public Optional<BannerVo> findByCategory(String category) {
+        return Optional.ofNullable(sqlSessionTemplate.selectOne("BANNER.findByCategory", category));
+    }
+
+    public String updateBanner(BannerVo vo) {
+        sqlSessionTemplate.update("BANNER.updateBanner", vo);
+        return vo.getCategory();
+    }
+
+    public String deleteFlower(String category) {
+        sqlSessionTemplate.delete("BANNER.deleteBanner", category);
+        return category;
+    }
 
 
 }
--- src/main/java/com/flo/alwaysbom/banner/service/BannerServiceImpl.java
+++ src/main/java/com/flo/alwaysbom/banner/service/BannerServiceImpl.java
@@ -1,11 +1,35 @@
 package com.flo.alwaysbom.banner.service;
 
+import com.flo.alwaysbom.banner.dao.BannerDao;
 import com.flo.alwaysbom.banner.vo.BannerVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class BannerServiceImpl implements BannerService {
 
+    private final BannerDao dao;
+
     @Override
-    public Integer addBanner(BannerVo bannerVo) {
-        return null;
+    public BannerVo addBanner(BannerVo vo) {
+        return dao.addBanner(vo);
+    }
+
+    @Override
+    public Optional<BannerVo> findByCategory(String category) {
+        return dao.findByCategory(category);
+    }
+
+    @Override
+    public String updateBanner(BannerVo vo) {
+        return dao.updateBanner(vo);
+    }
+
+    @Override
+    public String deleteFlower(String category) {
+        return dao.deleteFlower(category);
     }
 }
