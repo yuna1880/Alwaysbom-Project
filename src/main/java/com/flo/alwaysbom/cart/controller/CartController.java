a68d7d7ead080351678f52d642c3c1684039c6a5
cothis <minho1030@naver.com>
2021-04-09 11:23:28 +0900

    수정 : 버그 수정

--- src/main/java/com/flo/alwaysbom/cart/controller/CartController.java
+++ src/main/java/com/flo/alwaysbom/cart/controller/CartController.java
@@ -24,7 +24,7 @@ public class CartController {
     private final CartService cartService;
 
     @GetMapping("/cart/list")
-    public String getCart(@SessionAttribute(required = false) MemberVO member, Model model,) {
+    public String getCart(@SessionAttribute(required = false) MemberVO member, Model model) {
         if (member == null) {
             member = MemberVO.builder().id("test").build();
         }
