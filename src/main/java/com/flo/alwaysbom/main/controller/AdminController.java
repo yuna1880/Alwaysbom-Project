6fd07186c4733c5677194d38edd27fb993bcacf0
cothis <minho1030@naver.com>
2021-04-11 21:52:58 +0900

    수정 : 어드민 페이지 패스워드 암호화 기능 추가

--- src/main/java/com/flo/alwaysbom/main/controller/AdminController.java
+++ src/main/java/com/flo/alwaysbom/main/controller/AdminController.java
@@ -14,15 +14,21 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@SessionAttributes({"admin"})
+@SessionAttributes({"admin", "rsaWebKey"})
 public class AdminController {
 
     private final MainService mainService;
@@ -44,13 +50,37 @@ public class AdminController {
 
 
     @GetMapping("/admin/login")
-    public String goLogin() {
+    public String goLogin(Model model) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+
+        KeyPair keyPair = generator.genKeyPair();
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+        model.addAttribute("rsaWebKey", privateKey);
+        RSAPublicKeySpec publicSpec = keyFactory.getKeySpec(publicKey, RSAPublicKeySpec.class);
+
+        String publicModulus = publicSpec.getModulus().toString(16);
+        String publicExponent = publicSpec.getPublicExponent().toString(16);
+        model.addAttribute("publicModulus", publicModulus);
+        model.addAttribute("publicExponent", publicExponent);
+
         return "/admin/login";
     }
 
     @PostMapping("/admin/login")
-    public String loginProc(AdminVo adminVo, Model model) {
-        if (adminVo.getId().equals(adminVo.getPassword()) && adminVo.getId().equals("admin")) {
+    public String loginProc(@SessionAttribute PrivateKey rsaWebKey, AdminVo adminVo, Model model) throws Exception {
+        System.out.println("rsaWebKey = " + rsaWebKey);
+        System.out.println("adminVo = " + adminVo);
+        model.addAttribute("rsaWebKey", null);
+        String id = decryptRsa(rsaWebKey, adminVo.getId());
+        String password = decryptRsa(rsaWebKey, adminVo.getPassword());
+        System.out.println("id = " + id);
+        System.out.println("password = " + password);
+
+        if (id.equals("admin") && password.equals("admin")) {
+            adminVo = new AdminVo(id, password);
             model.addAttribute("admin", adminVo);
             return "redirect:/admin/main";
         } else {
@@ -92,5 +122,27 @@ public class AdminController {
         mainService.updateConfig(mainVo);
 
         return mainVo;
+    }
+
+    private String decryptRsa(PrivateKey privateKey, String securedValue) throws Exception {
+        System.out.println("will decrypt : " + securedValue);
+        Cipher cipher = Cipher.getInstance("RSA");
+        byte[] encryptedBytes = hexToByteArray(securedValue);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
+
+    private static byte[] hexToByteArray(String hex) {
+        if (hex == null || hex.length() % 2 != 0) {
+            return new byte[]{};
+        }
+
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < hex.length(); i += 2) {
+            byte value = (byte)Integer.parseInt(hex.substring(i, i + 2), 16);
+            bytes[(int) Math.floor(i / 2.0)] = value;
+        }
+        return bytes;
     }
 }
